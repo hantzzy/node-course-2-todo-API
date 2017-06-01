37decefe080d04fe99f657f1d1ef10353dbfa769
hantz alvarez <hantz@Oups.local>
2017-05-31 22:13:51 -0400

    add post dodo rout and refartoc mongonse

---
+++ server/models/todo.js
@@ -0,0 +1,22 @@
+/**
+ * Created by hantz on 5/2/17.
+ */
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+    text:{
+        type: String,
+        require:true,
+        minlength: 1,
+        trim: true
+    },
+    completed:{
+        type: Boolean,
+        default: false
+    },
+    completedAt:{
+        type: Number,
+        default: null
+    }});
+
+module.exports = ({Todo});
